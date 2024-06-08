f00be8a426aea866407f0b25955132c9ac1ded55
Zach Coyle <zach.coyle@gmail.com>
2024-06-08 01:28:33 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "nightly overlay for icon-themes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    systems.url = "github:nix-systems/default-linux";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+
+      systems = import inputs.systems;
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        devshells.default = {
+          name = "icon-themes-nightly-overlay";
+          packages = with pkgs; [
+            npins
+          ];
+        };
+      };
+      flake = {
+        overlays.default = final: prev: let in {};
+      };
+    };
+}
