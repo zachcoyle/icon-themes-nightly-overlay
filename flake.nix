5dc8b32e4caa1fd45fa80a7206c6d54cf5d3791d
Zach Coyle <zach.coyle@gmail.com>
2024-06-24 21:26:52 -0400

    nightly-date format for pkg names again

--- flake.nix
+++ flake.nix
@@ -14,41 +14,54 @@
     systems.url = "github:nix-systems/default-linux";
   };
 
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [
-        inputs.devshell.flakeModule
-      ];
+  outputs =
+    inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.devshell.flakeModule ];
 
       systems = import inputs.systems;
 
-      perSystem = {
-        config,
-        self',
-        inputs',
-        pkgs,
-        system,
-        ...
-      }: {
-        devshells.default = {
-          name = "icon-themes-nightly-overlay";
-          packages = with pkgs; [
-            npins
-          ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devshells.default = {
+            name = "icon-themes-nightly-overlay";
+            packages = with pkgs; [ npins ];
+          };
         };
-      };
       flake = {
-        overlays.default = final: prev: let
-          pins = import ./npins;
-          icon-themes = builtins.attrNames pins;
-        in (builtins.listToAttrs (map (name: {
-            inherit name;
-            value = prev.${name}.overrideAttrs {
-              src = pins.${name};
-              version = "nightly-${pins.${name}.revision}";
-            };
-          })
-          icon-themes));
+
+        overlays.default =
+          final: prev:
+          let
+            pins = import ./npins;
+            icon-themes = builtins.attrNames pins;
+            mkDate =
+              longDate:
+              (prev.lib.concatStringsSep "-" [
+                (builtins.substring 0 4 longDate)
+                (builtins.substring 4 2 longDate)
+                (builtins.substring 6 2 longDate)
+              ]);
+          in
+          (builtins.listToAttrs (
+            map (name: {
+              inherit name;
+              value = prev.${name}.overrideAttrs rec {
+                pname = name;
+                name = "${pname}-${version}";
+                src = pins.${name};
+                version = "nightly-${mkDate self.lastModifiedDate}";
+              };
+            }) icon-themes
+          ));
       };
     };
 }
